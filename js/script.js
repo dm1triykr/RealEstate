b25f92147477fbbca4dbb0fdd3e5d4295f63773b
dm1triykr <dimonk601@gmail.com>
2023-05-22 00:55:00 +0300

    burger menu

--- js/script.js
+++ js/script.js
@@ -33,3 +33,15 @@ document.addEventListener('click', function(e) {
         tabItems[newActiveIndex].classList.add('active');
     }
 });
+
+// ---------------------Burger---------------------
+
+const burgerBtn = document.querySelector('.icon-menu');
+if(burgerBtn) {
+    const menuBody = document.querySelector('.menu');
+    burgerBtn.addEventListener('click', function(e) {
+        document.body.classList.toggle('_lock');
+        burgerBtn.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
